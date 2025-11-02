c789ce04d23e79cf1721c4df58fc0717382e351c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-02 22:01:42 +0100

    Update esp_cam_sensor_xclk.h

--- components/esp_video/deps/include/esp_cam_sensor_xclk.h
+++ components/esp_video/deps/include/esp_cam_sensor_xclk.h
@@ -1,20 +1,21 @@
 #pragma once
+#include <stdint.h>
+#include "esp_err.h"
+#include "driver/gpio.h"
 
-typedef struct esp_cam_sensor_xclk_t *esp_cam_sensor_xclk_handle_t;
-typedef enum {
-    ESP_CAM_SENSOR_XCLK_MODE_GPIO = 0,
-    ESP_CAM_SENSOR_XCLK_MODE_LED_PWM,
-    ESP_CAM_SENSOR_XCLK_MODE_CLOCK_OUT,
-} esp_cam_sensor_xclk_mode_t;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-typedef struct {
-    int gpio_num;
-    unsigned int freq_hz;
-    esp_cam_sensor_xclk_mode_t mode;
+typedef struct esp_cam_sensor_xclk_config_t {
+    gpio_num_t xclk_pin;
+    uint32_t xclk_freq_hz;
 } esp_cam_sensor_xclk_config_t;
 
-static inline int esp_cam_sensor_new_xclk(const esp_cam_sensor_xclk_config_t *, esp_cam_sensor_xclk_handle_t *) { return 0; }
-static inline int esp_cam_sensor_del_xclk(esp_cam_sensor_xclk_handle_t) { return 0; }
-static inline int esp_cam_sensor_start_xclk(esp_cam_sensor_xclk_handle_t) { return 0; }
-static inline int esp_cam_sensor_stop_xclk(esp_cam_sensor_xclk_handle_t) { return 0; }
+esp_err_t esp_cam_sensor_xclk_init(esp_cam_sensor_xclk_config_t *config);
+esp_err_t esp_cam_sensor_xclk_deinit(void);
+
+#ifdef __cplusplus
+}
+#endif
 
